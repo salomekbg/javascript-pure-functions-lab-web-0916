3a3d89deb904ae8adca2628676c10b0c18e12d54
SluvsP <salomekbg@gmail.com>
2016-11-08 10:52:17 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,30 @@ const episodes = [
 ];
 
 const finaleEpisode = { id: 's06e10', title: 'The Winds of Winter' };
+
+function addToPlaylist(playlist, episode) {
+  return playlist.concat(episode);
+}
+
+function removeFromPlaylist(playlist, episode) {
+  var index = playlist.indexOf(episode);
+  return playlist.filter(function (element, i) {
+    return index !== i;
+  })
+  // var start = playlist.slice(0, index)
+  // var end = playlist.slice(index + 1, playlist.length)
+  // return start.concat(end)
+}
+
+function getNextEpisodeInPlaylist(playlist) {
+  return playlist[0];
+}
+
+function bingeWatch(playlist) {
+  if (playlist.length !== 0) {
+    var nextEp = getNextEpisodeInPlaylist(playlist);
+    console.log(nextEp)
+    bingeWatch(removeFromPlaylist(playlist, nextEp));
+  }
+  return "Please let there be more!";
+}
